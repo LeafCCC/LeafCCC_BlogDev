a690f609ba7cc91b26b175948f190a1fc726adff
LeafCCC <sjtu.zcc@outlook.com>
2022-03-18 20:00:40 +0800

    修改了config.js中网站的一些基本配置信息 如图标等; 修改了package.json中有关npm run dev的内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,19 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
+    title: 'LeafCCC\'s Blog (๑╹◡╹)ﾉ" ',
+    description: 'LeafCCC的个人网站',
+    head: [
+      ['link', { rel: 'icon', href: './images/logo.png' }] //设置标签处的图标 自己绘制了一个
+    ],
+    host: 'localhost', // ip
+    port: '8101', //端口号 默认为8080
+    themeConfig: {
+      logo: '/images/logo.png',// 注意图片放在 public 文件夹下
+      nav:[ // 导航栏配置
+        {text: '待定', link: '/accumulate/' },
+        {text: '待定', link: '/algorithm/'},
+        {text: 'My github', link: 'https://github.com/LeafCCC'}      
+      ],
+      sidebar: 'auto', // 侧边栏配置
+      sidebarDepth: 2, // 侧边栏显示2级
+    },
   }
