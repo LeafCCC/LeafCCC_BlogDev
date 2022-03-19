fa2dbe69f6bd34b0d17b328650afec718859a9a7
LeafCCC <sjtu.zcc@outlook.com>
2022-03-19 20:56:26 +0800

    修改了主页的内容 并且进行了部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: 'LeafCCC\'s Blog (๑╹◡╹)ﾉ" ',
-    description: 'LeafCCC的个人网站',
+    title: 'Hello, I\'m LeafCCC! (๑╹◡╹)ﾉ" ',
+    description: 'LeafCCC的个人介绍网站',
     head: [
       ['link', { rel: 'icon', href: './images/logo.png' }] //设置标签处的图标 自己绘制了一个
     ],
@@ -8,12 +8,19 @@ module.exports = {
     port: '8101', //端口号 默认为8080
     themeConfig: {
       logo: '/images/logo.png',// 注意图片放在 public 文件夹下
+      
       nav:[ // 导航栏配置
-        {text: '待定', link: '/accumulate/' },
-        {text: '待定', link: '/algorithm/'},
-        {text: 'My github', link: 'https://github.com/LeafCCC'}      
+      { 
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items:[
+          { text: 'English', link: '/language/English/' },
+          { text: '中文', link: '/language/Chinese/' }
+        ]    
+       },
+       {text: 'My github', link: 'https://github.com/LeafCCC'},   
       ],
-      sidebar: 'auto', // 侧边栏配置
-      sidebarDepth: 2, // 侧边栏显示2级
+      
+      
     },
   }
